701a79984233538ac3f6ec36a54b2e30143f0994
Anubhav Srivastava <misteranubhav@gmail.com>
2019-09-03 21:50:52 -0700

    Cleaned up code, added basic timing.

--- testopencv/Source.cpp
+++ testopencv/Source.cpp
@@ -1,47 +1,44 @@
+#include <ctime>
 #include <iostream>
 #include <utility>
 #include <string>
+
+#include <opencv2/opencv.hpp>
+
 #include "segm/msImageProcessor.h"
 #include "edge/BgEdgeDetect.h"
 #include "edge/BgImage.h"
-#include <opencv2/opencv.hpp>
 
 using namespace std;
-int main() {
-	//parameters for mean shift
-	const int sigmaS = 7;           //spatial bandwidth
-	const float sigmaR = 6.5;       //range bandwidth
-	const int minRegion = 20;       //area of the smallest objects to consider
-	//parameters for synergistic segmentation
-	const int gradWindRad = 2;      //Gradient Window Radius
-	const float threshold = 0.3F;   //Edge Strength Threshold [0,1]
-	const float mixture = 0.2F;     //Mixture Parameter [0,1]
-	const SpeedUpLevel speedup = MED_SPEEDUP;
 
-	// Load the image (as BGR) then convert to RGB
+int main() {
+	std::clock_t start;
+	int duration;
+
+	//parameters for mean shift
+	const int sigmaS = 7;           // spatial bandwidth
+	const float sigmaR = 6.5;       // range bandwidth
+	const int minRegion = 20;       // area of the smallest objects to consider
+	//parameters for synergistic segmentation
+	const int gradWindRad = 2;      // gradient window radius
+	const float threshold = 0.3F;   // Edge strength threshold [0,1]
+	const float mixture = 0.2F;     // mixture parameter [0,1]
+	const SpeedUpLevel speedup = SpeedUpLevel::MED_SPEEDUP;
+
+	// Load the image as BGR (src_bgr) then convert to RGB (src)
 	const cv::Mat src_bgr = cv::imread("rose.png");
 	cv::Mat src;
 	cv::cvtColor(src_bgr, src, cv::COLOR_BGR2RGB);
-	assert(src.data != nullptr);
 
-	// Convert to char buffer
+	// Compute edges
 	const int height = src.rows;
 	const int width = src.cols;
-	uchar* image_buf;
-	if (src.isContinuous()) {
-		image_buf = src.data;
-	}
-	else {
-		cerr << "Image not continuous. TODO" << endl;
-		return 1;
-	}
-
 	vector<float> gradMap(width * height);
 	vector<float> confMap(width * height);
 	vector<float> weightMap(width * height);
 	BgEdgeDetect edgeDetector(gradWindRad);
 	BgImage bgImage;
-	bgImage.SetImage(image_buf, width, height, true);
+	bgImage.SetImage(src.data, width, height, true);
 	edgeDetector.ComputeEdgeInfo(&bgImage, confMap.data(), gradMap.data());
 
 	// Compute weight map
@@ -54,8 +51,11 @@ int main() {
 		}
 	}
 
+	// Filter and fuse regions
 	msImageProcessor iProc;
-	iProc.DefineImage(image_buf, COLOR, height, width);
+	//printf("Filtering and fusing: ");
+	//start = clock();
+	iProc.DefineImage(src.data, COLOR, height, width);
 	assert(!iProc.ErrorStatus);
 	iProc.SetWeightMap(weightMap.data(), threshold);
 	assert(!iProc.ErrorStatus);
@@ -63,25 +63,34 @@ int main() {
 	assert(!iProc.ErrorStatus);
     iProc.FuseRegions(sigmaR, minRegion);
 	assert(!iProc.ErrorStatus);
+	//duration = (clock() - start) * 1000 / (double)CLOCKS_PER_SEC;
+	//printf("%d ms\n", duration);
 
+	// The memory for the following arrays is managed by msImageProcessor.
+	// width * height sized array mapping each pixel to a label identifying a
+	// region (i.e. a segment). Stored in row major order, as usual.
 	int* labels;
+	// regionCount * 3 sized array. Stores the color value for each region in
+	// LUV space sequentially (for each region, stores 3 floats for L, U, and V).
+	// e.g. the eighth region is at indices 3*8 + 0, 3*8 + 1, and 3*8 + 2.
 	float* modes;
+	// regionCount sized array mapping region label to the number of points in it.
 	int* modePointCounts;
 	const int regionCount = iProc.GetRegions(&labels, &modes, &modePointCounts);
 
-	// Can be uncommented if you want to visualize the final image
-	//vector<float> final_image_luv_vec(height * width * 3);
+	// Can be uncommented if you want to visualize the final segmented image.
+	//vector<float> finalImageLuvVec(height * width * 3);
 	//for (int i = 0; i < height * width; ++i) {
 	//	const int label = labels[i];
 	//	for (int dim = 0; dim < 3; ++dim) {
-	//		final_image_luv_vec[i * 3 + dim] = modes[labels[i] * 3 + dim];
+	//		finalImageLuvVec[i * 3 + dim] = modes[labels[i] * 3 + dim];
 	//	}
 	//}
-	//cv::Mat final_image_luv(height, width, CV_32FC3, final_image_luv_vec.data());
-	//cv::Mat final_image_bgr;
-	//cv::cvtColor(final_image_luv, final_image_bgr, cv::COLOR_Luv2BGR);
-	//cv::imshow("Source Image", final_image_bgr);
+	//cv::Mat finalImageLuv(height, width, CV_32FC3, finalImageLuvVec.data());
+	//cv::Mat finalImageBgr;
+	//cv::cvtColor(finalImageLuv, finalImageBgr, cv::COLOR_Luv2BGR);
+	//cv::imshow("Image", finalImageBgr);
+	//cv::waitKey(0);
 
-	cv::waitKey(0);
 	return 0;
 }
